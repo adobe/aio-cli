706bf7ea96ee703d8f941b8752d8c5961adce8e6
Jesse MacFadyen <purplecabbage@gmail.com>
2019-03-05 15:47:01 -0800

    semi cleanup

--- src/index.js
+++ src/index.js
@@ -10,12 +10,15 @@
  * governing permissions and limitations under the License.
  */
 
-const { run } = require('@oclif/command')
+const { Command, run } = require('@oclif/command')
 const Config = require("@oclif/config")
 
-const preRun = async function (argv, opts) {
+
+class AIOCommand extends Command { }
+
+AIOCommand.run =  async (argv, opts) => {
   if (!argv) {
-    argv = process.argv.slice(2);
+    argv = process.argv.slice(2)
   }
 
   // oclif originally included the following too ... 
@@ -24,27 +27,27 @@ const preRun = async function (argv, opts) {
     //||  module.parent && module.parent.parent && module.parent.parent.filename 
   const config = await Config.load(opts || __dirname);
 
-  let subCommand = argv[0];
+  let subCommand = argv[0]
 
   // 1. find the first flag ( note: there could be none ... )
-  let firstFlag = argv.slice().findIndex(elem => elem.indexOf('-') == 0);
+  let firstFlag = argv.slice().findIndex(elem => elem.indexOf('-') == 0)
   if (firstFlag < 0) {
-    firstFlag = argv.length;
+    firstFlag = argv.length
   }
   // 2. try to make the biggest topic command by combining with ':'
   // and looking up in this.config.commandIDs
   for (let x = firstFlag; x > -1; x--) {
-    subCommand = argv.slice(0, x).join(':');
+    subCommand = argv.slice(0, x).join(':')
       //if (config.findTopic(subCommand)) { // <= this works but does not support aliases
     if (config.findCommand(subCommand)) {
-      argv = [subCommand].concat(argv.slice(x));
-      break;
+      argv = [subCommand].concat(argv.slice(x))
+      break
     }
   }
 
   // the second parameter is the root path to the CLI containing the command
   return run(argv, config.options)
 
-};
+}
 
-module.exports = { run: preRun }
+module.exports = {run:AIOCommand.run}
