df5adeae67f1b98d154a0a9c1f522af589e90f44
Shazron Abdullah (shaz) <shazron@gmail.com>
2019-11-28 16:52:35 +0800

    Interim tests

--- test/commands/discover.test.js
+++ test/commands/discover.test.js
@@ -1,32 +1,24 @@
 
-const testCommand = require('../../src/commands/discover')
+const DiscoverCommand = require('../../src/commands/discover')
 
-// jest.mock('node-fetch', (url) => {
-//   return {
-//     'cool': 'you bet'
-//   }
-// })
+jest.mock('node-fetch', (url) => {
+  const now = new Date().valueOf()
+  return {
+    results: [
+      { package: { name: 'foo', version: '1.0.0', date: new Date(now + 100) } },
+      { package: { name: 'bar', version: '1.0.1', date: new Date(now + 200) } }
+    ]
+  }
+})
 
-// jest.mock('@oclif/command', () => {
-//   return {
-//     // note: arrow function won't work because Command is extended and called with new
-//     Command: function () {},
-//     run: function (cmd) {
-//       return cmd
-//     }
-//   }
-// })
+test('exports a run function', async () => {
+  expect(typeof DiscoverCommand.run).toEqual('function')
+})
 
-describe('discover command', () => {
-  test('exports a run function', async () => {
-    expect(typeof testCommand.run).toEqual('function')
-  })
+test('discover - unknown sort field', async () => {
+  expect.assertions(2)
 
-  // test('run function returns a promise', async () => {
-  //   let result = testCommand.run()
-  //   expect(typeof result).toEqual('object')
-  //   expect(typeof result.then).toEqual('function')
-  //   expect(typeof result.catch).toEqual('function')
-  // })
-  
+  const runResult = DiscoverCommand.run(['--sort-field', 'unknown'])
+  await expect(runResult instanceof Promise).toBeTruthy()
+  await expect(runResult).rejects.toThrow('xxx') // TODO: probe for actual error
 })
