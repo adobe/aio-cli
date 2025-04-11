4fdb56bfad38464388f4cbda07adc4224629ca66
Deepty Thampy <dthampy@adobe.com>
2025-04-11 18:57:18 -0500

    chore: add a warning when non-supported node version is used

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@
  */
 
 const { Command, run, Config } = require('@oclif/core')
+const semver = require('semver');
+const chalk = require('chalk');
 
 class AIOCommand extends Command { }
 
@@ -24,6 +26,17 @@ AIOCommand.run = async (argv, opts) => {
   // seem like it would only
   // ||  module.parent && module.parent.parent && module.parent.parent.filename
   const config = await Config.load(opts || __dirname)
+
+  // Check Node.js version
+  const nodeVersion = process.version;
+  if (semver.major(nodeVersion) > 22) {
+    console.log(chalk.yellow('⚠️ Warning: You are using Node.js version ' + nodeVersion + ', which is not officially supported by Adobe I/O CLI.'));
+    console.log(chalk.yellow('   Supported versions are Node.js 18, 20, and 22.'));
+    console.log(chalk.yellow('   To use a supported Node.js version, please consider using nvm (Node Version Manager).'));
+    console.log(chalk.yellow('   To find the nvm documentation visit:'));
+    console.log(chalk.yellow('   https://github.com/nvm-sh/nvm'));
+    console.log();
+  }
 
   // the second parameter is the root path to the CLI containing the command
   try {
